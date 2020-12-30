5a5d1da931cc725be01af24b72ea92452bc9b05d
Michael Kluge <vollseil@gmail.com>
2020-12-30 22:19:16 +0100

    fixed suggestions from pylint and documentaion

--- iot_control/iotruntime.py
+++ iot_control/iotruntime.py
@@ -6,8 +6,8 @@
 """
 
 import time
-import yaml
 import sys
+import yaml
 import iot_control.iot_devices.iotads1115
 import iot_control.iot_devices.iotbme280
 import iot_control.iot_devices.iotbh1750
@@ -18,7 +18,10 @@ from iot_control.iotfactory import IoTFactory
 
 
 class IoTRuntime:
-
+    """ this is the runtime class that will load the setup
+        and put backends and sensors together and runs the
+        main loop
+    """
     backends = []
     devices = []
     update_intervall = 60
@@ -37,9 +40,8 @@ class IoTRuntime:
             try:
                 self.backends.append(IoTFactory.create_backend(
                     backend, config=backend_cfg))
-            except:
-                e = sys.exc_info()[0]
-                print("error creating backend: {}".format(e))
+            except Exception as exception:
+                print("error creating backend: {}".format(exception))
         sys.stdout.flush()
         # second: register devices with backend
         for device in self.conf["devices"]:
@@ -52,18 +54,25 @@ class IoTRuntime:
                 self.devices.append(real_device)
                 for backend in self.backends:
                     backend.register_device(real_device)
-            except:
-                e = sys.exc_info()[0]
-                print("error creating device: {}".format(e))
+            except Exception as exception:
+                print("error creating device: {}".format(exception))
         sys.stdout.flush()
         for backend in self.backends:
             backend.announce()
         sys.stdout.flush()
 
     def set_intervall(self, new_intervall: int):
+        """ sets the intervall between two readings of the sensors
+
+        Args:
+            new_intervall (int): the new interval in seconds between
+            readings of the sensors
+        """
         self.update_intervall = new_intervall
 
     def loop_forever(self):
+        """ the main loop of the runtime
+        """
         while True:
             for device in self.devices:
                 data = device.read_data()

5707880c47496f35d3834f73847a51fc37c31a1b
Michael Kluge <vollseil@arcor.de>
2021-01-01 16:17:50 +0100

    there are different bme280 python modules, use rpi.bme280

--- iot_control/iot_devices/iotbme280.py
+++ iot_control/iot_devices/iotbme280.py
@@ -26,8 +26,8 @@ class IoTbme280(IoTDeviceBase):
     def read_data(self) -> Dict:
         """ read data """
         bus = smbus2.SMBus(self.port)
-#        calibration_params = bme280.load_calibration_params(
-#            bus, self.address)
+        calibration_params = bme280.load_calibration_params(
+            bus, self.address)
         data = bme280.sample(bus, self.address)
         val = {
             "temperature": "{:.1f}".format(data.temperature),
